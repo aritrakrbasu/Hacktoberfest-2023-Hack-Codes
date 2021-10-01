4b926b0dd948c0136d7ce9b90590d4d3e7deac96
Rachel Sanham <green_chul@hotmail.com>
2021-10-01 21:02:23 +0100

    Added Opposite Number challange to CodeWars

---
+++ Codewars/Opposite-Number/solution.js
@@ -0,0 +1,9 @@
+// Author: Rachel aka greenchul (greensand on CodeWars)
+// Question link: https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+function opposite(number) {
+    //find the opposite number by adding "-"
+    let opp = 0 - number;
+    // return oppsite number
+    return opp
+  }
