0952f76534e2c05c4d4a126a93af577e92df83db
Shourya Shikhar <danger.ahead@pm.me>
2021-10-02 08:39:57 +0530

    added-KADANES_ALGO-GFG

---
+++ GeeksforGeeks/KadanesAlgo/solution.java
@@ -0,0 +1,52 @@
+import java.io.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int cur=0, max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            cur=cur+arr[i];
+            if(cur>max){
+                max=cur;
+            }
+            if(cur<0){
+                cur=0;
+            }
+        }
+        return max;
+    }
+    
+}
+
