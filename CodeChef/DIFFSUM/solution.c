0bf04fc6d5a4f5278783ba59da2104937385e9a6
Aritra Banerjee <aritra.ban11@gmail.com>
2021-10-03 14:07:21 +0530

    added DiffSum solution

---
+++ CodeChef/DIFFSUM/solution.c
@@ -0,0 +1,19 @@
+//author: Aritra Banerjee
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a,b;
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+	    printf("%d",a-b);
+	}
+	else
+	{
+	    printf("%d",a+b);
+	}
+	return 0;
+}
+
