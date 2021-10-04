709c7e1e73afbc278752d7264733f39c6a2c75fd
ARITRA BELEL <82683890+belelaritra@users.noreply.github.com>
2021-10-04 15:23:13 +0530

    LeetCode : 5 Linked List Question Added #9
    Issue No. #9
    5 Linked List Questions and Solutions from LeetCode Added
    with a beautiful Documentation.

---
+++ LeetCode/Linked List/Delete_Node_in_a_Linked_List.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (!node || !node->next) {
+            return;
+        }
+        auto node_to_delete = node->next;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        delete node_to_delete;
+    }
+};
---
+++ LeetCode/Linked List/Remove_Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp = head;
+        while ((temp!=NULL)&&(temp->next!=NULL)){
+            if (temp->val == temp->next->val){
+                temp->next=temp->next->next;
+            }
+            else{
+            temp=temp->next;}
+        }
+        return head;
+    }
+};
