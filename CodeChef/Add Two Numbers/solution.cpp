adc88cca7674aa9db0f47f633a4e2ea145c77dd0
Saheli Thakur <53173563+SaheliThakur@users.noreply.github.com>
2021-10-06 12:59:35 +0530

    Add two numbers

---
+++ CodeChef/Add Two Numbers/solution.cpp
@@ -0,0 +1,22 @@
+//Author: Saheli Thakur
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int T;
+    scanf("%d", &T);
+    while (T--)
+    {
+
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        int ans = a + b;
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
