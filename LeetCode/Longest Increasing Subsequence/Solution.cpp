ed5908421f7aaac7733fae76f251f2fa03aa18c5
Sagnikban <banerjeesagnik29@gmail.com>
2021-10-07 12:36:46 +0530

    LIS

---
+++ LeetCode/Longest Increasing Subsequence/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n+1,1);
+
+        dp[0]=1;
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[j]<nums[i])
+                dp[i]=max(dp[i],dp[j]+1);
+            }
+        }
+
+        return *max_element(dp.begin(),dp.end());
+
+    }
+};
