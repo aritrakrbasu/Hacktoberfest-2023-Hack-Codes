1957942511fe4f312ee06449c52fb333f612cf8f
Snowborn0600 <63629219+Snowborn0600@users.noreply.github.com>
2021-10-08 17:46:39 +0530

    Added Multi Level Inheritance

---
+++ HackerRank/Multi Level Inheritance/Solution.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Triangle{
+	public:
+		void triangle(){
+			cout<< "I am a triangle\n";
+		}
+};
+
+class Isosceles : public Triangle{
+  	public:
+  		void isosceles(){
+    		cout<<" I am an isosceles triangle\n";
+  		}
+};
+
+//Write your code here.
+
+int main(){
+  
+    Equilateral eqr;
+    eqr.equilateral();
+    eqr.isosceles();
+    eqr.triangle();
+    return 0;
+}
