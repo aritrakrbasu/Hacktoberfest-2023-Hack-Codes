d36c3159f76303486bb547c9b307a185c85d16c7
Runa Basu <runabasu71@gmail.com>
2021-10-09 15:44:33 +0530

    no message

--- LeetCode/House Robber/solution.cpp
+++ LeetCode/House Robber/solution.cpp
@@ -1,16 +1,26 @@
 class Solution {
-    public int rob(int[] nums) {
-        int n =nums.length;
-        if(n==0)return 0;
-        if(n==1)return nums[0];
-        if(n==2)return Math.max(nums[0],nums[1]);
-        
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
-        }
-        return dp[n-1];
+public:
+    int rob(vector<int>& nums) {
+        int dp[nums.size()+1];
+
+        if(nums.size()==1)
+        return nums[0];
+
+       if(nums.size()==2)
+        return max(nums[0],nums[1]);
+
+         if(nums.size()==2)
+        return max(nums[2]+nums[0],nums[1]);
+
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        dp[2]=max(nums[2]+nums[0],nums[1]);
+
+        for(int i=3;i<nums.size();i++)
+        dp[i]=nums[i]+max(dp[i-2],dp[i-3]);
+
+
+        return max(dp[nums.size()-1],dp[nums.size()-2]);
+
     }
-}
+};
