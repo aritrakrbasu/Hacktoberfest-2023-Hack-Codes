b6d05e1adc83a7ee860a315c7ddaaece52676b94
diya-beep <diya41010@gmail.com>
2021-10-18 23:22:31 +0530

    My Python Solution

---
+++ HackerRank/No Idea!/SOLUTION.py
@@ -0,0 +1,21 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Python 3
+io = input().split()
+m = int(io[0])
+n = int(io[1])
+
+storage = list()
+count = 0
+
+storage = list(map(int, input().strip().split()))
+
+A = set(map(int, input().strip().split()))
+B = set(map(int, input().strip().split()))
+
+for i in storage:
+    if i in A:
+        count = count+1
+    if i in B:
+        count = count-1
+
+print(count)
