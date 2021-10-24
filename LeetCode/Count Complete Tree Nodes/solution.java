bbf7ac014a72ee00ab713163f56c941c916f96f0
Moulina Pradhan <pmoulina76@gmail.com>
2021-10-24 18:24:53 +0530

    add  solution

---
+++ LeetCode/Count Complete Tree Nodes/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   int count=0;
+    public int countNodes(TreeNode root) {
+      inorder(root);
+        return count;
+    }
+    public void inorder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+count++;
+        inorder(root.right);
+    }
+}
