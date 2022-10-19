1d7f72ece859bab42b9a622feee7c690e5768ce3
Kankona-C <kankona01chowdhury@gmail.com>
2022-10-19 20:53:17 +0530

    Added solution to BIRYANI problem

---
+++ CodeChef/BIRYANI/solution.java
@@ -0,0 +1,22 @@
+// author: Kankona Chowdhury
+
+import java.util.;
+import java.lang.;
+import java.io.;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++){
+
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+
+            System.out.println(xy);
+        }
+    }
+}
